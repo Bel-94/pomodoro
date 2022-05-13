a7c9e1064ff83c2f3508ec2cf677b575a1e54809
Joy Christine <joychristin2@gmail.com>
2022-05-13 13:22:22 +0300

    initial commit

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,8 @@ from flask_bootstrap import Bootstrap
 
 db = SQLAlchemy()
 bootstrap = Bootstrap()
+
+
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
 login_manager.login_view = 'auth.login'
--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS=True
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://amiani:pass2022@localhost/belinda'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://pomo:mypassword@localhost/pomodoro'
     SECRET_KEY = 'MODIFY'
 
 class ProdConfig(Config):
